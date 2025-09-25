c4b0b9130e6a25b6f315aece277f041c69db9fc2
Eiseul <foaming21@gmail.com>
2025-09-25 10:17:18 +0900

    [Bronze V] Title: 사분면 고르기, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -7,14 +7,21 @@ public class Main {
         int x = sc.nextInt();
         int y = sc.nextInt();
 
-        if(x>0 && y>0){
-            System.out.println("1");
-        }else if(x<0 && y>0){
-            System.out.println("2");
-        }else if(x<0 && y<0){
-            System.out.println("3");
-        }else{
-            System.out.println("4");
+        // 입력 검증 코드
+        if (x < -1000 || x > 1000 || x == 0) {
+            return;
+        } else if (y < -1000 || y > 1000 || y == 0) {
+            return;
+        }
+
+        if (x > 0 && y > 0) {               // 둘 다 양수인 경우
+            System.out.print("1");
+        } else if (x < 0 && y > 0) {        // x가 음수, y가 양수인 경우
+            System.out.print("2");
+        } else if (x < 0 && y < 0) {        // 둘 다 음수인 경우
+            System.out.print("3");
+        } else {                            // x가 양수, y가 음수인 경우
+            System.out.print("4");
         }
     }
 }
